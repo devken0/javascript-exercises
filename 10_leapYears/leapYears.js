d1ef178192048b97be81808290e62956ebbbbba7
devken0 <119792185+devken0@users.noreply.github.com>
2025-05-12 22:58:26 +0800

    Complete leapYears exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+function isDivisibleBy(dividend, divisor){
+    return dividend % divisor === 0;
+}
 
+const leapYears = function(year) {
+    if (isDivisibleBy(year, 100) && !isDivisibleBy(year, 400)){
+        return false;
+    } else if (!isDivisibleBy(year, 4)){
+        return false;
+    } else return true;
 };
 
 // Do not edit below this line
