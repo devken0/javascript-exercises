56eb24875013e1febade68b4a74f3caf5ec61e03
devken0 <119792185+devken0@users.noreply.github.com>
2025-05-12 22:57:55 +0800

    Complete reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(myString) {
+    let char, result = "";
+    for (i = 1; i <= myString.length; i++){
+        result += myString.at(-i);
+    }
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
