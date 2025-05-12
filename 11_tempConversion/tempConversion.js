e3ee264f096c293a1db1b2782dfdd15af3b2e363
devken0 <119792185+devken0@users.noreply.github.com>
2025-05-12 22:58:40 +0800

    Complete tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(currentTemp) {
+  const result = (currentTemp - 32) * (5/9);
+  return round(result, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(currentTemp) {
+  const result = currentTemp * (9/5) + 32;
+  return round(result, 1);
 };
 
 // Do not edit below this line
