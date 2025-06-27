2c07e0ca0ab492288f121c77cce75f2ad4513b74
devken0 <119792185+devken0@users.noreply.github.com>
2025-06-27 17:31:51 +0800

    Complete palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const ALPHANUMERICAL = "abcdefhijklmnopqrstuvwxyz1234567890";
 
+  const cleanedString = string.toLowerCase().split('').filter((character) => ALPHANUMERICAL.includes(character)).join('');
+  const reversedString = cleanedString.split('').reverse().join('');
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
